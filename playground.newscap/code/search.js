db460fa4b7b5ec48307b6c175610af058a40a5cd
xyzmnw <76397823+xyzmnw@users.noreply.github.com>
2021-08-03 21:10:59 +0530

    Add files via upload

---
+++ playground.newscap/code/search.js
@@ -0,0 +1,29 @@
+http=require("http")
+var console = require("console")
+module.exports.function = function search (category,language,country) {
+var url = "https://newsapi.org/v2/top-headlines?apiKey=497523214d6e42cb92a8b8f22ef22960"   
+  var options={
+    "Accept" : "application/json"
+  }
+   var ret= {}
+   if(typeof(category) != "undefined")
+   {
+     options["category"]=category
+   }
+   if(typeof(language) != "undefined")
+   {
+     options["language"]=language
+   }
+   if(typeof(country) != "undefined")
+   {
+     options["country"]=country
+   }
+   try{
+   res = http.getUrl(url,{query:options})
+   ret=JSON.parse(res)
+   }catch(err){
+     console.log("Error: "+err)
+
+   }
+  return ret
+}
